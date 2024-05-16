0eeb5ed70e2b93ed567d6f4f4d42fa0fd3fc36ed
1Sig <Sigurdowre@gmail.com>
2024-05-16 09:47:20 +0200

    fixed something

--- handlers/tokenhandeler.js
+++ handlers/tokenhandeler.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const tokenSecret = process.env.TOKENSECRET;
 
 function generateAccessToken(username, user_id){
-    return jwt.sign({username, user_id}, tokenSecret, {expiresIn:"1d"});
+    return jwt.sign({username, _id: user_id}, tokenSecret, {expiresIn:"1d"});
 }
 
 module.exports={
